c561757ae46b3ece1030bc81a131cd15f1cc7be0
장원덕 <dnjsejr417@outlook.com>
2024-11-11 13:22:58 +0900

    backoff 테스트 추가

--- ThreadTest/ThreadTest.c
+++ ThreadTest/ThreadTest.c
@@ -51,7 +51,8 @@ void init_lock(Lock* lock) {
 // atomic_exchange : 변수의 값을 새 값으로 설정하고 이전 값을 원자적으로 반환(단일 교환 연산이 필요한 경우 사용한다)
 
 void tas_lock(Lock* lock) {
-	while (atomic_exchange(&lock->state, 1) == 1) {}
+	int expected = 0;
+	while (atomic_compare_exchange_weak(&lock->state, &expected, 1)) {}
 }
 
 void ttas_lock(Lock* lock) {
@@ -65,6 +66,21 @@ void ttas_lock(Lock* lock) {
 			}
 		}
 		// 락 획득 실패 시 반복 시도
+	}
+}
+
+void back_off_lock(Lock* lock) {
+	while (true) {
+		// 첫 번째 테스트: 상태가 0인지 확인
+		if (atomic_load(&lock->state) == 0) {
+			// 두 번째 테스트: 비교 후 교환 시도
+			int expected = 0;
+			if (atomic_compare_exchange_weak(&lock->state, &expected, 1)) {
+				break; // 락 획득 성공
+			}
+		}
+		// 락 획득 실패 후 일정 시간 대기
+		Sleep(1);
 	}
 }
 
@@ -91,6 +107,18 @@ int ttas_add(void* arg) {
 
 	for (int i = data->start; i <= data->end; ++i) {
 		ttas_lock(data->lock);
+		sum += i;
+		unlock(data->lock);
+	}
+
+	return 0;
+}
+
+int back_off_add(void* arg) {
+	ThreadData* data = (ThreadData*)arg;
+
+	for (int i = data->start; i <= data->end; ++i) {
+		back_off_lock(data->lock);
 		sum += i;
 		unlock(data->lock);
 	}
@@ -424,6 +452,169 @@ void ttas_test() {
 	printf("TTASLock Sum: %llu\n", sum);
 }
 
+void back_off_test() {
+	// 2개의 스레드를 사용한 TASLock 테스트
+	clock_t start;
+	clock_t end;
+	thrd_t threads[SIXTYFOUR];
+	Lock lock;
+	init_lock(&lock);
+
+	// 각각의 스레드에 전달할 데이터 설정 
+	ThreadData data[SIXTYFOUR];
+	data[0] = init_ThreadData(&lock, 1000000, 1000000 + 4000000 / TWO);
+	for (int i = 1; i < TWO; ++i) {
+		data[i] = init_ThreadData(&lock, 1000000 + (4000000 / TWO * i) + 1, 1000000 + (4000000 / TWO * (i + 1)));
+	}
+
+	sum = 0;
+	for (int i = 0; i < TWO; ++i) {
+		if (thrd_create(&threads[i], back_off_add, &data[i]) != thrd_success) {
+			printf("Error creating thread 1\n");
+			return;
+		}
+	}
+	start = clock();
+	for (int i = 0; i < TWO; ++i) {
+		thrd_join(threads[i], NULL);
+	}
+	end = clock();
+
+	printf("%d threads\n", TWO);
+	printf("TTASLock Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("TTASLock Sum: %llu\n", sum);
+
+	// 4개의 스레드를 사용한 TASLock 테스트
+	init_lock(&lock);
+
+	// 각각의 스레드에 전달할 데이터 설정 
+	data[0] = init_ThreadData(&lock, 1000000, 1000000 + 4000000 / FOUR);
+	for (int i = 1; i < FOUR; ++i) {
+		data[i] = init_ThreadData(&lock, 1000000 + (4000000 / FOUR * i) + 1, 1000000 + (4000000 / FOUR * (i + 1)));
+	}
+
+	sum = 0;
+	for (int i = 0; i < FOUR; ++i) {
+		if (thrd_create(&threads[i], back_off_add, &data[i]) != thrd_success) {
+			printf("Error creating thread 1\n");
+			return;
+		}
+	}
+	start = clock();
+	for (int i = 0; i < FOUR; ++i) {
+		thrd_join(threads[i], NULL);
+	}
+	end = clock();
+
+	printf("%d threads\n", FOUR);
+	printf("TTASLock Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("TTASLock Sum: %llu\n", sum);
+
+	// 8개의 스레드를 사용한 TASLock 테스트
+	init_lock(&lock);
+
+	// 각각의 스레드에 전달할 데이터 설정 
+	data[0] = init_ThreadData(&lock, 1000000, 1000000 + 4000000 / EIGHT);
+	for (int i = 1; i < EIGHT; ++i) {
+		data[i] = init_ThreadData(&lock, 1000000 + (4000000 / EIGHT * i) + 1, 1000000 + (4000000 / EIGHT * (i + 1)));
+	}
+
+	sum = 0;
+	for (int i = 0; i < EIGHT; ++i) {
+		if (thrd_create(&threads[i], back_off_add, &data[i]) != thrd_success) {
+			printf("Error creating thread 1\n");
+			return;
+		}
+	}
+	start = clock();
+	for (int i = 0; i < EIGHT; ++i) {
+		thrd_join(threads[i], NULL);
+	}
+	end = clock();
+
+	printf("%d threads\n", EIGHT);
+	printf("TTASLock Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("TTASLock Sum: %llu\n", sum);
+
+	// 16개의 스레드를 사용한 TASLock 테스트
+	init_lock(&lock);
+
+	// 각각의 스레드에 전달할 데이터 설정 
+	data[0] = init_ThreadData(&lock, 1000000, 1000000 + 4000000 / SIXTEEN);
+	for (int i = 1; i < SIXTEEN; ++i) {
+		data[i] = init_ThreadData(&lock, 1000000 + (4000000 / SIXTEEN * i) + 1, 1000000 + (4000000 / SIXTEEN * (i + 1)));
+	}
+
+	sum = 0;
+	for (int i = 0; i < SIXTEEN; ++i) {
+		if (thrd_create(&threads[i], back_off_add, &data[i]) != thrd_success) {
+			printf("Error creating thread 1\n");
+			return;
+		}
+	}
+	start = clock();
+	for (int i = 0; i < SIXTEEN; ++i) {
+		thrd_join(threads[i], NULL);
+	}
+	end = clock();
+
+	printf("%d threads\n", SIXTEEN);
+	printf("TTASLock Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("TTASLock Sum: %llu\n", sum);
+
+	// 32개의 스레드를 사용한 TASLock 테스트
+	init_lock(&lock);
+
+	// 각각의 스레드에 전달할 데이터 설정 
+	data[0] = init_ThreadData(&lock, 1000000, 1000000 + 4000000 / THIRTYTWO);
+	for (int i = 1; i < THIRTYTWO; ++i) {
+		data[i] = init_ThreadData(&lock, 1000000 + (4000000 / THIRTYTWO * i) + 1, 1000000 + (4000000 / THIRTYTWO * (i + 1)));
+	}
+
+	sum = 0;
+	for (int i = 0; i < THIRTYTWO; ++i) {
+		if (thrd_create(&threads[i], back_off_add, &data[i]) != thrd_success) {
+			printf("Error creating thread 1\n");
+			return;
+		}
+	}
+	start = clock();
+	for (int i = 0; i < THIRTYTWO; ++i) {
+		thrd_join(threads[i], NULL);
+	}
+	end = clock();
+
+	printf("%d threads\n", THIRTYTWO);
+	printf("TTASLock Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("TTASLock Sum: %llu\n", sum);
+
+	// 64개의 스레드를 사용한 TASLock 테스트
+	init_lock(&lock);
+
+	// 각각의 스레드에 전달할 데이터 설정 
+	data[0] = init_ThreadData(&lock, 1000000, 1000000 + 4000000 / SIXTYFOUR);
+	for (int i = 1; i < SIXTYFOUR; ++i) {
+		data[i] = init_ThreadData(&lock, 1000000 + (4000000 / SIXTYFOUR * i) + 1, 1000000 + (4000000 / SIXTYFOUR * (i + 1)));
+	}
+
+	sum = 0;
+	for (int i = 0; i < SIXTYFOUR; ++i) {
+		if (thrd_create(&threads[i], back_off_add, &data[i]) != thrd_success) {
+			printf("Error creating thread 1\n");
+			return;
+		}
+	}
+	start = clock();
+	for (int i = 0; i < SIXTYFOUR; ++i) {
+		thrd_join(threads[i], NULL);
+	}
+	end = clock();
+
+	printf("%d threads\n", SIXTYFOUR);
+	printf("TTASLock Time: %f\n", (double)(end - start) / CLOCKS_PER_SEC);
+	printf("TTASLock Sum: %llu\n", sum);
+}
+
 int main(void) {
 	clock_t start;
 	clock_t end;
@@ -447,6 +638,8 @@ int main(void) {
 	tas_test();
 	printf("\n===TTASLock test===\n");
 	ttas_test();
+	printf("\n===Back-off test===\n");
+	back_off_test();
 
 	return 0;
 }
